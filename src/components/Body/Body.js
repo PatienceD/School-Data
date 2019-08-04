ac5fec6057ecf753162690bf0c1cdd047fba0fb8
Patience D <patience.deaton2@outlook.com>
2019-08-04 19:33:51 -0400

    charts pulling data from API and displaying correctly

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,31 +1,19 @@
 import React, { Component } from "react";
-// import { BrowserRouter as Router } from "react-router-dom";
-// var CanvasJSReact = require('./canvasjs.react');
-// var CanvasJS = CanvasJSReact.CanvasJS;
-// var CanvasJSChart = CanvasJSReact.CanvasJSChart;
-
+import { Doughnut } from 'react-chartjs-2';
+import { Polar } from 'react-chartjs-2';
 var axios = require("axios");
 let apiURL =
     "https://api.data.gov/ed/collegescorecard/v1/schools/?school.operating=1&2015.academics.program_available.assoc_or_bachelors=true&2015.student.size__range=1..&school.degrees_awarded.predominant__range=1..3&school.degrees_awarded.highest__range=2..4&id=240444&api_key=mkIYGU6R65A5fNgNLr2uaaywY4pEEuhDGkyt0oDG";
 
 
-/* GET users listing. */
-// router.get("/", function (req, res, next) {
-//     axios
-//         .get(apiURL)
-//         .then(response => {
-//             res.send(response.data.results[0].school.name);
-//         })
-//         .catch(error => {
-//             res.status(error).send("Error on request");
-//         });
-// });
 
 class School extends Component {
     constructor(props) {
         super(props);
-
         this.state = { schoolName: " " };
+        this.state = {
+            Charts: []
+        }
     }
 
     componentDidMount() {
@@ -33,15 +21,27 @@ class School extends Component {
     }
 
 
-
     SchoolName() {
         axios
             .get(apiURL)
             .then(response => {
-                    this.setState({
-                        Name: response.data.results[0].school.name
-                    })
-                    console.log(response.data.results[0].school.name);
+                this.setState({
+                    Name: response.data.results[0].school.name,
+                    Address: response.data.results[0].school.city + " " + response.data.results[0].school.state + " " + response.data.results[0].school.zip,
+                    Students: response.data.results[0].school.alias,
+                    URL: response.data.results[0].school.school_url,
+                    Black: response.data.results[0].latest.student.demographics.race_ethnicity.black,
+                    Asian: response.data.results[0].latest.student.demographics.race_ethnicity.asian,
+                    White: response.data.results[0].latest.student.demographics.race_ethnicity.white,
+                    Hispanic: response.data.results[0].latest.student.demographics.race_ethnicity.hispanic,
+                    Math: response.data.results[0].latest.academics.program_percentage.mathematics,
+                    Psychology: response.data.results[0].latest.academics.program_percentage.psychology,
+                    Communication: response.data.results[0].latest.academics.program_percentage.communication,
+                    Health: response.data.results[0].latest.academics.program_percentage.health,
+                    Biological: response.data.results[0].latest.academics.program_percentage.biological,
+
+                })
+                console.log(response.data.results[0].latest.student.demographics.race_ethnicity.black)
             })
             .catch(error => {
                 console.log("Error on request");
@@ -50,32 +50,58 @@ class School extends Component {
 
 
 
-
     render() {
+        const EthicityData = {
+            labels: [
+                'Black',
+                'Asian',
+                'White',
+                'Hispanic'
+            ],
+            datasets: [{
+                data: [this.state.Black, this.state.Asian, this.state.White, this.state.Hispanic],
+                backgroundColor: [
+                    '#FF6384',
+                    '#36A2EB',
+                    '#FFCE56',
+                    '#009900'
+                ],
+                hoverBackgroundColor: [
+                    '#FF6384',
+                    '#36A2EB',
+                    '#FFCE56',
+                    '#009900'
+                ]
+            }]
+        };
 
-        // const options = {
-        // 	exportEnabled: true,
-        // 	animationEnabled: true,
-        // 	title: {
-        // 		text: "Website Traffic Sources"
-        // 	},
-        // 	data: [{
-        // 		type: "pie",
-        // 		startAngle: 75,
-        // 		toolTipContent: "<b>{label}</b>: {y}%",
-        // 		showInLegend: "true",
-        // 		legendText: "{label}",
-        // 		indexLabelFontSize: 16,
-        // 		indexLabel: "{label} - {y}%",
-        // 		dataPoints: [
-        // 			{ y: 18, label: "Direct" },
-        // 			{ y: 49, label: "Organic Search" },
-        // 			{ y: 9, label: "Paid Search" },
-        // 			{ y: 5, label: "Referral" },
-        // 			{ y: 19, label: "Social" }
-        // 		]
-        // 	}]
-        // }
+        const ProgramData = {
+            labels: [
+                'Mathematics',
+                'Psychology',
+                'Communication',
+                'Health',
+                "Biological"
+            ],
+            datasets: [{
+                data: [this.state.Math, this.state.Psychology, this.state.Communication, this.state.Health, this.state.Biological],
+                backgroundColor: [
+                    '#FF6384',
+                    '#36A2EB',
+                    '#FFCE56',
+                    "#009900",
+                    '#000066'
+                ],
+                hoverBackgroundColor: [
+                    '#FF6384',
+                    '#36A2EB',
+                    '#FFCE56',
+                    '#009900',
+                    '#000066'
+                ]
+            }]
+        };        
+
 
         const { error } = this.state;
         if (error) {
@@ -84,35 +110,32 @@ class School extends Component {
             return (
                 <div className="container">
                     <div className="row">
-                        <div class="jumbotron" style={{ marginTop: "25px" }}>
-                            <h2 class="display-4">{this.state.Name}</h2>
-                            <p class="lead">Location: Madison, Wisconsin 53706</p>
-                            <p>Current Alias:</p>
+                        <div className="jumbotron" style={{ marginTop: "25px" }}>
+                            <h2 className="display-4">{this.state.Name}</h2>
+                            <p className="lead">Location: {this.state.Address}</p>
+                            <p>Current Alias: No Current Alias</p>
                             <p>Student Count:</p>
-                            <hr class="my-4"></hr>
+                            <hr className="my-4"></hr>
                             <p>"The primary purpose of the University of Wisconsin–Madison is to provide a learning environment in which faculty,
                                 staff and students can discover, examine critically, preserve and transmit the knowledge, wisdom and values that
                         will help ensure the survival of this and future generations and improve the quality of life for all."</p>
-                            <a class="btn btn-lg" href="#" role="button" style={{ backgroundColor: "#DAA520" }}>Check out their website</a>
+                            <a className="btn btn-lg" href={this.state.URL} role="button" rel="noopener noreferrer" style={{ backgroundColor: "#DAA520" }} target="_blank">Check out their website</a>
                         </div>
                     </div>
                     <div className="row">
                         <div className="col-md-6">
                             <div className="card">
                                 <div className="card-body">
-
-                                    {/* <CanvasJSChart options={options} /> */}
-
-                                    {/* <img src="http://via.placeholder.com/150"></img>
-                                    <h5>Pie Chart Display</h5> */}
+                                    <h4 style={{ textAlign: "center" }}>Academics at UOW</h4>
+                                    <Doughnut data={ProgramData} />
                                 </div>
                             </div>
                         </div>
                         <div className="col-md-6">
                             <div className="card">
                                 <div className="card-body">
-                                    <img src="http://via.placeholder.com/150"></img>
-                                    <h5>Donut Chart Display</h5>
+                                    <h4 style={{ textAlign: "center" }}>Ethicity at UOW</h4>
+                                    <Doughnut data={EthicityData} />
 
                                 </div>
                             </div>
@@ -122,8 +145,8 @@ class School extends Component {
                         <div className="col-md-6">
                             <div className="card">
                                 <div className="card-body">
-                                    <img src="http://via.placeholder.com/150"></img>
-                                    <h5>Pie Chart Display</h5>
+                                    <h4 style={{ textAlign: "center" }}>This is the title</h4>
+                                    <Polar data={PolarData} />
                                 </div>
                             </div>
                         </div>
@@ -132,20 +155,19 @@ class School extends Component {
                                 <div className="card-body">
                                     <div className="row">
                                         <div className="col-md-12">
-                                            <button type="button" class="btn" style={{ backgroundColor: "#DAA520" }}>Save Page as a PDF</button>
+                                            <button type="button" className="btn" style={{ backgroundColor: "#DAA520" }}>Save Page as a PDF</button>
                                         </div>
                                     </div>
                                     <div className="row">
                                         <div className="col-md-12">
-                                            <button type="button" class="btn" style={{ backgroundColor: "#DAA520" }}>Download</button>
+                                            <button type="button" className="btn" style={{ backgroundColor: "#DAA520" }}>Download</button>
                                         </div>
                                     </div>
                                     <div className="row">
                                         <div className="col-md-12">
-                                            <button type="button" class="btn" style={{ backgroundColor: "#DAA520" }}>Print</button>
+                                            <button type="button" className="btn" style={{ backgroundColor: "#DAA520" }}>Print</button>
                                         </div>
                                     </div>
-
                                 </div>
                             </div>
                         </div>
@@ -155,5 +177,33 @@ class School extends Component {
         }
     }
 }
+
+
+const PolarData = {
+    datasets: [{
+        data: [
+            11,
+            16,
+            7,
+            3,
+            14
+        ],
+        backgroundColor: [
+            '#FF6384',
+            '#4BC0C0',
+            '#FFCE56',
+            '#E7E9ED',
+            '#36A2EB'
+        ],
+        label: 'My dataset' // for legend
+    }],
+    labels: [
+        'Red',
+        'Green',
+        'Yellow',
+        'Grey',
+        'Blue'
+    ]
+};
 
 export default School;
