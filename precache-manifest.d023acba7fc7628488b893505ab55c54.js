429c1d2dd6e7ef08fbfbea582575e679ea863942
Patience Deaton <46998183+PatienceD@users.noreply.github.com>
2019-12-02 12:18:54 -0500

    changed file structure

---
+++ precache-manifest.d023acba7fc7628488b893505ab55c54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3fe43be3e8fbe6dd8d7cd4da5de08a2",
+    "url": "/Institutional-Metadata/index.html"
+  },
+  {
+    "revision": "154e656183f674ad12f6",
+    "url": "/Institutional-Metadata/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "4003d2e98bd78dd26797",
+    "url": "/Institutional-Metadata/static/js/2.4e6e63a9.chunk.js"
+  },
+  {
+    "revision": "154e656183f674ad12f6",
+    "url": "/Institutional-Metadata/static/js/main.5078f2e2.chunk.js"
+  },
+  {
+    "revision": "839043b71c45a55ef71b",
+    "url": "/Institutional-Metadata/static/js/runtime-main.8b33ea27.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Institutional-Metadata/static/media/logo.25bf045c.svg"
+  }
+]);
